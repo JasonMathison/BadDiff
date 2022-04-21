4f8429ad13c4209ec375f67f60a3a797b6368088
JasonMathison <JasonMathison@users.noreply.github.com>
2022-04-21 12:14:14 -0400

    Update code.java
    Remove a line and remove static from all variables.

--- code.java
+++ code.java
@@ -1,4 +1,3 @@
-protected static String deleteMe = "I am going to be deleted";
-protected static Var1 var1;
-protected static Var2 Var2;
-protected static Var3 Var3;
+protected Var1 var1;
+protected Var2 Var2;
+protected Var3 Var3;
